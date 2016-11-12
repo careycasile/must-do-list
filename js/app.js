3a90f09aade3ffcc352583f9bd97bb37df4ddfde
Carey <carey.casile@gmail.com>
2016-11-11 21:33:38 -0500

    updated markers display

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+//Global map variable is defined
 var map;
 
+//Initial Data is placed inside objects inside an array
 var information = [{
 		name: 'Kennywood Park',
 		address: '4800 Kennywood Blvd, West Mifflin, PA 15122',
@@ -137,29 +139,73 @@ var information = [{
 		direction: 236.01
 	}];
 
+//InitMap function is called from the index.html file towards the bottom inside the googleapis.com... src script tag
 var InitMap = function() {
 	'use strict'
 	var self = this;
+	//The map is drawn on the page here
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 40.440187, lng: -79.9778809},
 		zoom: 12,
 		styles: [{"elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#f5f5f2"},{"visibility":"on"}]},{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","stylers":[{"visibility":"off"}]},{"featureType":"poi.school","stylers":[{"visibility":"off"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#ffffff"},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"visibility":"simplified"},{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"color":"#ffffff"},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.park","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#71c8d4"}]},{"featureType":"landscape","stylers":[{"color":"#e5e8e7"}]},{"featureType":"poi.park","stylers":[{"color":"#8ba129"}]},{"featureType":"road","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.sports_complex","elementType":"geometry","stylers":[{"color":"#c7c7c7"},{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#a0d3d3"}]},{"featureType":"poi.park","stylers":[{"color":"#91b65d"}]},{"featureType":"poi.park","stylers":[{"gamma":1.51}]},{"featureType":"road.local","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","stylers":[{"visibility":"simplified"}]},{"featureType":"road"},{"featureType":"road"},{},{"featureType":"road.highway"}]		
 	});
 
-	this.allPins = function() {
-		console.log('all pins fired');
-		for (var x = 0; x < fullArray.length; x++) {
-			var latitude = fullArray[x].lat;
-			var longitude = fullArray[x].lng;
+	//The function in the MapKeeper class, allPins, is called to draw the initial markers on the map
+	mk.allPins();
 
+	//The vm.filterList function is called to let the knockout ViewModel know to start tracking the filter
+	//field
+	vm.filterList();
+};	
+
+//The MapKeeper class keeps track of the map elements and works directly with the ViewModel to update as needed
+var MapKeeper = function () {
+	var markerArray = [];
+
+	//All pins is called once and prepares each item to be placed on the map. This function does not actually 
+	//place the items as that is taken care of in the modifyPins... functions.
+	this.allPins = function() {
+		'use strict'
+		for (var x = 0; x < vm.fullArray.length; x++) {
+			var latitude = vm.fullArray[x].lat;
+			var longitude = vm.fullArray[x].lng;
 			var marker = new google.maps.Marker({
 			  	position: {lat: latitude , lng: longitude},
-			  	map: map
+			  	//animation: google.maps.Animation.DROP
 			});
+			markerArray.push(marker);
 		}
 	};
-	this.allPins();
 
+	//Removes all current pins from the map
+	this.removePins = function() {
+		'use strict'
+		for (var x = 0; x < markerArray.length; x++){
+				markerArray[x].setMap(null);
+		}
+	};
+
+	//Modifies a pin to show and bounce when selected
+	this.modifyPinsSelect = function(num) {
+		markerArray[num].setMap(map);
+		markerArray[num].setAnimation(google.maps.Animation.BOUNCE);
+	};
+
+	//Adds a pin to the map
+	this.modifyPinsPlus = function(num) {
+		markerArray[num].setMap(map);
+		markerArray[num].setAnimation(google.maps.Animation.DROP);
+	};
+
+	//removes a pin from the map
+	this.modifyPinsMinus = function(num) {
+		markerArray[num].setMap(null);
+	};
+
+
+	this.pinItem = function(data){
+
+	};
 };
 
 //MODEL
@@ -176,6 +222,7 @@ var InfoItem = function(data) {
 	this.icon = data.icon;
 	this.direction = data.direction;
 	this.visible = ko.observable(true);
+	this.id = null;
 	//If loop matches a font awesome icon to the item's icon type
 	if (this.icon === 'att') {
 		self.display = '<i class="fa fa-camera" aria-hidden="true"></i>';
@@ -189,46 +236,51 @@ var InfoItem = function(data) {
 	} 
 };
 
-var clicked = function(data){
-	'use strict'
 
-	for (var x = 0; x < fullArray.length; x++) {
-		fullArray[x].setmap(null);
-	}
-};
 
 //VIEWMODEL
-var fullArray = [];
+
 var ViewModel = function(){
 	'use strict'
 	var self = this;
 
 	//All Data items
-
+	this.fullArray = [];
 	for (var x = 0; x < information.length; x++) {
-		fullArray.push(new InfoItem(information[x]));
+		var tempItem = new InfoItem(information[x])
+		tempItem.id = x;
+		this.fullArray.push(tempItem);
 	}
 
 	//TODO item is clicked, google map populates info
 	this.itemClick = function() {
 		'use strict'
-		clicked(this);
+		mk.removePins();
+		mk.modifyPinsSelect(this.id);
+		mk.pinItem(this);
 	};
 
 	this.userSubmission = ko.observable('');
 
-   	this.filterList = ko.computed(function() {
-   		for (var x = 0; x < fullArray.length; x++) {
-   			var submission = self.userSubmission();
-   			var arrayItem = fullArray[x].descriptionIcon.indexOf(submission.toLowerCase());
-   			if (arrayItem !== -1){
-   				fullArray[x].visible(true);
-   			} else {
-   				fullArray[x].visible(false);
-			}
-   		}
-    });
+   	this.filterList = function(){
+   		ko.computed(function() {
+	   		for (var x = 0; x < self.fullArray.length; x++) {
+	   			var submission = self.userSubmission();
+	   			var arrayItem = self.fullArray[x].descriptionIcon.indexOf(submission.toLowerCase());
+	   			if (arrayItem !== -1){
+	   				self.fullArray[x].visible(true);
+	   				mk.modifyPinsPlus(x);
+	   			} else {
+	   				self.fullArray[x].visible(false);
+	   				mk.modifyPinsMinus(x);
+				}
+	   		}
+	    });
+   	};
 };
 
 
-ko.applyBindings(new ViewModel());
+
+var mk = new MapKeeper();
+var vm = new ViewModel();
+ko.applyBindings(vm);
